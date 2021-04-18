68e973a373463e062b1e0e33ee2faadd0e4ce984
Harshavardhana <75427218+Harshavardhana505@users.noreply.github.com>
2021-04-18 15:39:33 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,104 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what do you call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["help me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["what is html"],
+   ["html"],
+   ["define  html"],
+   ["what is css","define css","css"],
+   ["what is javascript","javascript","define javascript"],
+   ["what are the most famous programming languages","Name some programming languages","programming languages","programming languages in the world"],
+   ["yippee","hurray"],
+   ["Corona","Define corona","do you have covid?","covid","define covid","I have covid what should i do","I have corona what should i do","I am sick"],   	       
+   ["haha","ha","lol","hehe","funny","joke"],
+   ["Emojis please"],
+   ["i have a question","can you help me"],
+   ["do you get smarter"],
+   ["some scientists","scientists"],
+   ["what topic should we talk about","what shall we talk about"],
+   ["GRR I D"]
+     
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["ChatterP", "ChatterP's my name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why?", "Try watching TV"],
+  ["Two fish in a tank. One says: How do you drive this thing?","Why did the chicken cross the road? To get to the other side!  Why did the duck cross the road? As it was chicken's day off!! Hahahah! ","How many employees did it take to fix a light bulb?6 people. 3 to fix the bulb 3 to stitch the uniform!!! "],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand"],
+  ["Please say something :("],
+   ["Html is a webpage programming Language. Learn more:http:// w3schools.com"],
+   ["Html is a webpage programming Language. Learn more: http://w3schools.com"],
+   ["Html is a webpage programming Language. Learn more: http://w3schools.com"],
+   ["Css is a Stylesheet Lang for Html. Ask me what html is "],
+   ["JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites. (For html) Ask me what html is!"],
+   ["HTML   CSS Python  Java  JavaScript  Swift C++ C# R Golang (Go) Php C  Asp.net"],
+   ["Hurray","Why?","What is the excitement all about?"],
+   ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"],
+   ["Haha!","Good one!"],
+   ["😍️😁️😂️😋️😀️😚️😥️😫️😱️😥️👾️🤖️💪️👌️🤘️👎️🍉️🍇️🌍️🎥❌🎬🌜🌛🌒🌘🌗🌕🌀🌊🛴♻⨁⊝⊞💣💬⚠️"],
+   ["Yes! How?","Yes, What is your problem"],
+   ["Yes everyday I learn new sentences.","Everyday"],
+   ["Albert Einstein"]
+   ["Albert Einstein"],
+   ["I don't know. How about outer space? Ask me a question about it."],
+   ["Pluto","Saturn","Jupiter"]
+
+      
+   ]
+
+// Random for any other user input
+
+const alternative = [
+"I don't understand :/ I'm still learning. I'm Still not a know all robot"
+]
+
+
+// Whatever else you want :)
+
+const programmingLang = ["http://w3schools.com", "http://geeksforgeeks.com", "http://stackoverflow.com", "http://github.com","html.com"]
